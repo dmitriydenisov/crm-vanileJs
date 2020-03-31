8985c8909657460714da7247e20374b48f66fa3f
dmitriydenisov <dmitriyd2009@yandex.ru>
2020-03-31 20:05:38 +0400

    Дописал функцию addLastReviewed с проверкой orderId на уникальность записей

--- src/Database.js
+++ src/Database.js
@@ -2,35 +2,18 @@
     "use script"
 
     const database = {
+        lastReviewed: {
+            maxLength: 4,
+            orderIds: [1, 2, 3, 4]
+        },
         orders: [
-            {
-                id: 1,
-                fullname: "Дмитрий Денисов",
-                good: "Бумага для принтера",
-                price: 500,
-                status: "new",
-                date: Date.now()
-            },
-            {
-                id: 2,
-                fullname: "Алексей Данчин",
-                good: "Краска для принтера",
-                price: 1500,
-                status: "process",
-                date: Date.now()
-            },
-            {
-                id: 3,
-                fullname: "Илья Муромец",
-                good: "Принтер",
-                price: 11500,
-                status: "back",
-                date: Date.now()
-            }
+            { id: 1, fullname: "Дмитрий Денисов", good: "Бумага для принтера", price: 500, status: "new", date: Date.now() },
+            { id: 2, fullname: "Алексей Данчин", good: "Краска для принтера", price: 1500, status: "process", date: Date.now() },
+            { id: 3, fullname: "Илья Муромец", good: "Принтер", price: 11500, status: "back", date: Date.now() }
         ]
     }
 
-    const api = {}
+    const api = new EventEmitter
 
     api.generate = function generate (orders) {
         database.orders = getCopy(orders)
@@ -44,6 +27,27 @@
         }
 
         return null
+    }
+
+    //функция возврата последних заказов
+    api.getLastReviewed = function getLastReviewed () {
+        return database.lastReviewed.orderIds.map(x => api.getOrderById(x))
+    }
+
+    api.addLastReviewed = function addLastReviewed (orderId) {
+        //если есть такая запись
+        if(database.lastReviewed.orderIds.includes(orderId)) { 
+            //мы ее ищем 
+            const splice = database.lastReviewed.orderIds.indexOf(orderId)     
+            //удаляем
+            database.lastReviewed.orderIds.splice(splice, 1)
+            // перезаписываем в верхней строчке
+            database.lastReviewed.orderIds.unshift(orderId)
+        }else{
+            database.lastReviewed.orderIds = [orderId, ...database.lastReviewed.orderIds].slice(0, database.lastReviewed.maxLength)
+        }     
+
+        api.emit('update')
     }
 
     window.Database = api
